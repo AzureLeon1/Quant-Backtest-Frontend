697afb16138ab472dce1484996ea77d7f1bb7ceb
Leon <leonwangchn@163.com>
2020-06-23 23:31:45 +0800

    websocket

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -32,13 +32,22 @@ export default {
           default: null
       }
    },
+   created() {
+      // this.initWebSocket();
+   },
    mounted() {
        console.log(this.strategy_id);
        // TODO: 请求回测数据
    },
+   destroyed() {
+      //this.websock.close() //离开路由之后断开websocket连接
+    },
   data () {
     return {
-      columns1: [
+        websocket: null,
+        result: {},
+        temp_time: "",
+        columns1: [
           {
               title: '回测时间',
               key: 'time',
@@ -98,7 +107,7 @@ export default {
          console.log(row.strategy_id);
      },
      run() {
-         console.log('run!');
+         /* console.log('run!');
          console.log(this.value);
          var params = {
             "user_id": sessionStorage.getItem('user'),
@@ -110,12 +119,44 @@ export default {
         this.$axios.post('./api/backtest', params).then((response) => {
             var res = response.data
             console.log(response);
-            
-            
         }).catch((error) => {
             this.$message.error("回测失败，请稍后重试")
-        })
-     }
+        }) */
+        this.initWebSocket();
+     },
+     initWebSocket(){ //初始化weosocket
+        //const wsuri = "ws://localhost/api/wsbacktest?nickName=" + "leon";
+        this.temp_time = getNowTime()
+        const wsuri = "ws://localhost:44370/api/wsbacktest?code=" + this.value + "&strategy_id=" + this.strategy_id + "&time=" + this.temp_time;
+        this.websock = new WebSocket(wsuri);
+        this.websock.onmessage = this.websocketonmessage;
+        this.websock.onopen = this.websocketonopen;
+        this.websock.onerror = this.websocketonerror;
+        this.websock.onclose = this.websocketclose;
+     },
+      websocketonopen(){ //连接建立之后执行send方法发送数据
+/*         let actions = {"test":"12345"};
+        this.websocketsend(JSON.stringify(actions)); */
+        console.log("opened");
+      },
+      websocketonerror(){//连接建立失败重连
+      console.log("error");
+        this.initWebSocket();
+      },
+      websocketonmessage(e){ //数据接收
+      console.log(e.data);
+      console.log(JSON.parse(e.data));
+        this.result = JSON.parse(e.data);
+        this.result.time = this.temp_time
+        this.data1.push(this.result)
+        console.log("message");
+      },
+      websocketsend(Data){//数据发送
+        this.websock.send(Data);
+      },
+      websocketclose(e){  //关闭
+        console.log('断开连接',e);
+      },
     }
 }
 </script>
